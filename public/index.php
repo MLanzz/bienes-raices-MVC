d8f2749a3a9269c24a523bf614aabc87f21ec4fd
Maximiliano Lanz <maximilianolanz@outlook.com.ar>
2023-04-22 23:57:45 -0300

    Paginas publicas
    Comienzo con las paginas que no necesitan inicio de sesión

--- public/index.php
+++ public/index.php
@@ -5,23 +5,32 @@
     use MVC\Router;
     use Controllers\PropiedadController;
     use Controllers\VendedorController;
+    use Controllers\PaginasController;
 
     $router = new Router();
 
-
+    // Se requiere estar autenticado para visitar estas urls
     $router->get("/admin", [PropiedadController::class, "index"]);
     $router->get("/propiedades/crear", [PropiedadController::class, "crear"]);
     $router->get("/propiedades/actualizar", [PropiedadController::class, "actualizar"]);
-    
     $router->post("/propiedades/actualizar", [PropiedadController::class, "actualizar"]);
     $router->post("/propiedades/eliminar", [PropiedadController::class, "eliminar"]);
     $router->post("/propiedades/crear", [PropiedadController::class, "crear"]);
-
     $router->get("/vendedores/crear", [VendedorController::class, "crear"]);
     $router->get("/vendedores/actualizar", [VendedorController::class, "actualizar"]);
-
     $router->post("/vendedores/actualizar", [VendedorController::class, "actualizar"]);
     $router->post("/vendedores/eliminar", [VendedorController::class, "eliminar"]);
     $router->post("/vendedores/crear", [VendedorController::class, "crear"]);
     
+    // No se requiere estar autenticado para visitar estas urls
+    $router->get("/", [PaginasController::class, "index"]);
+    $router->get("/nosotros", [PaginasController::class, "index"]);
+    $router->get("/propiedades", [PaginasController::class, "propiedades"]);
+    $router->get("/propiedad", [PaginasController::class, "propiedad"]);
+    $router->get("/blog", [PaginasController::class, "blog"]);
+    $router->get("/entrada", [PaginasController::class, "entrada"]);
+    $router->get("/contacto", [PaginasController::class, "contacto"]);
+    $router->post("/contacto", [PaginasController::class, "contacto"]);
+
+
     $router->comprobarRutas();
