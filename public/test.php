ff077d1f2a3c9d69929aeabe6a05c2b7a8713524
Maximiliano Lanz <maximilianolanz@outlook.com.ar>
2023-04-26 19:50:49 -0300

    Propiedades
    Fix para la pantalla de propiedades
    Las descripciones se estaban mostrando mal, agrego css para fixearlo

--- public/test.php
+++ public/test.php
@@ -1,2 +1,23 @@
 <?php 
 require_once __DIR__ . "/../includes/app.php";
+
+// $password = "123456";
+// $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+// $query = "INSERT INTO users (username, password) VALUES ('username', '" . $passwordHash . "')";
+
+// debuguear($query);
+
+
+// $db = new mysqli("34.73.158.52", "administrador", "mYN5qGwrEw8bR78J8kDGKmS7nx8kDp", "test_codigo");
+
+// if(!$db) {
+//     echo "Error no se pudo conectar";
+//     exit;
+// }
+
+
+// $query = "SELECT * FROM users";
+
+// $resultado = $db->query($query);
+
+// debuguear($resultado->fetch_assoc());
--- views/paginas/listado.php
+++ views/paginas/listado.php
@@ -5,7 +5,7 @@
 
             <div class="contenido-anuncio">
                 <h3><?php echo $propiedad->titulo; ?></h3>
-                <!-- <p><?php echo $propiedad->descripcion; ?></p> -->
+                <p><?php echo trim($propiedad->descripcion); ?></p>
                 <p class="precio">
                     $<?php echo $propiedad->precio; ?>
                 </p>
